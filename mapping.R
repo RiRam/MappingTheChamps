e16b7e99bede415d7ec6b60cdab20461af564ee6
Ri Ram <eram1294@gmail.com>
2016-10-28 17:15:03 -0400

    added figures

--- mapping.R
+++ mapping.R
@@ -3,6 +3,7 @@ library(ggmap)
 library(maps)
 library(mapdata)
 library(readr)
+library(dplyr)
 
 states <- map_data("state")
 world <- map_data("world")
@@ -12,7 +13,7 @@ state_counts <- read_csv("state_counts.csv")
 states <- left_join(states, state_counts, by = "region")
 
 ggplot(data = states) + 
-  geom_polygon(aes(x = long, y = lat, fill=as.factor(count), group = group), color = "white") +
+  geom_polygon(aes(x = long, y = lat, fill = as.factor(count), group = group), color = "white") +
   guides(fill=guide_legend(title="Champ Count")) +
   ggtitle("Number of Champs Per State") +
   coord_quickmap()
@@ -22,7 +23,7 @@ country_counts <- read_csv("country_counts.csv")
 world <- left_join(world, country_counts, by = "region")
 
 ggplot(data = world) + 
-  geom_polygon(aes(x = long, y = lat, fill=as.factor(count), group = group), color = "white") +
+  geom_polygon(aes(x = long, y = lat, fill = as.factor(count), group = group), color = "white") +
   guides(fill=guide_legend(title="Champ Count")) +
   ggtitle("Number of Champs Per Country") +
   coord_quickmap()
